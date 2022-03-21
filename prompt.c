b0cf73df0ec72cf3f45e8ee505ca1a559d54bcc1
Heather Rothery <hrothery@student.42wolfsburg.de>
2022-03-21 13:58:55 +0100

    more builtins

--- prompt.c
+++ prompt.c
@@ -6,7 +6,7 @@
 /*   By: hrothery <hrothery@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 16:24:57 by hrothery          #+#    #+#             */
-/*   Updated: 2022/03/19 18:52:48 by hrothery         ###   ########.fr       */
+/*   Updated: 2022/03/21 13:49:16 by hrothery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,13 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
+#include "libft/libft.h"
 
 #define GRN  "\x1B[1;32m"
 #define BLU  "\x1B[1;34m"
 #define NRM  "\x1B[0m"
-#define MAX_LENGTH 100
 
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strrchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	if (c == '\0')
-		return ((char *)(s + ft_strlen(s)));
-	while (s[i] != '\0')
-		i++;
-	while (i >= 0)
-	{
-		if (s[i] == (char)c)
-			return ((char *)(s + i));
-		i--;
-	}
-	return (NULL);
-}
+void	parse_builtin(char *line, char **envp, int	*exit_status);
 
 void	display_prompt(void)
 {
@@ -70,17 +40,17 @@ void	display_prompt(void)
 }
 
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	int	background;
+	int	exit_status;
 	char *line;
 
-	background = 0;
-	while (background == 0)
+	exit_status = 0;
+	while (exit_status == 0)
 	{
 		//display_prompt();
-		line = readline("\033[1;34m\033[1mMinishell\033[0m$ ");
-		parse_builtin(line);
+		line = readline("\033[1;36m\033[1mMinishell\033[0m$ ");
+		parse_builtin(line, envp, &exit_status);
 		add_history(line);
 		free(line);
 	}
