446091794ce98b483d14239ad6be9cca5481aba5
cfabian <cfabian@student.42wolfsburg.de>
2022-05-14 22:00:14 +0200

    fixed catcatls

--- src/piping.c
+++ src/piping.c
@@ -6,7 +6,7 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 08:42:52 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/14 13:09:50 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/14 21:56:21 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,19 @@ static void	parent_process(t_pdata pdata, t_command *cmd_s)
 	close_two(pdata.oldpipe[0], pdata.oldpipe[1]);
 	dup2_and_close(pdata.newpipe[0], pdata.oldpipe[0]);
 	dup2_and_close(pdata.newpipe[1], pdata.oldpipe[1]);
-	if (cmd_s->next && cmd_s->cmd && \
+	if (!cmd_s->next)
+	{
+		while (true)
+		{
+			if (waitpid (-1, &g_last_exit, 0) <= 0)
+				break ;
+		}
+	}
+	/*if (cmd_s->next && cmd_s->cmd && \
 	(ft_strcmp(cmd_s->cmd[0], "cat") || cmd_s->cmd[1]))
 		waitpid(pdata.pid, &g_last_exit, WNOHANG);
 	else
-		waitpid(pdata.pid, &g_last_exit, 0);
+		waitpid(pdata.pid, &g_last_exit, 0);*/
 	g_last_exit = g_last_exit / 255;
 }
 
@@ -55,14 +63,22 @@ void	child_p(t_pdata pd, t_envvar *env_l, t_envvar *exp_l, t_command *cmd_s)
 			dup2_and_close(cmd_s->fd_out, pd.newpipe[1]);
 		dup2_and_close(pd.newpipe[1], STDOUT_FILENO);
 		close_two(pd.newpipe[0], pd.oldpipe[1]);
-		own_env = ft_listtostr(env_l);
-		execve(cmd_s->cmd[0], cmd_s->cmd, own_env);
-		path = joined_path(pd.paths, cmd_s->cmd[0]);
-		if (path)
-			execve(path, cmd_s->cmd, own_env);
-		free(path);
-		ft_double_free(own_env);
-		print_error(cmd_s->cmd[0], ": command not found\n", NULL, NULL);
+		if (is_builtin(cmd_s->cmd))
+		{
+			perror(cmd_s->cmd[0]);
+			parse_builtin(cmd_s, env_l, exp_l);
+		}
+		else
+		{
+			own_env = ft_listtostr(env_l);
+			execve(cmd_s->cmd[0], cmd_s->cmd, own_env);
+			path = joined_path(pd.paths, cmd_s->cmd[0]);
+			if (path)
+				execve(path, cmd_s->cmd, own_env);
+			free(path);
+			ft_double_free(own_env);
+			print_error(cmd_s->cmd[0], ": command not found\n", NULL, NULL);
+		}
 	}
 	free_everything(env_l, exp_l, cmd_s);
 	exit(127);
