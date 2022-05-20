f7a5561049afd845504d62a03469be83dfa1656a
Heather Rothery <hrothery@student.42wolfsburg.de>
2022-05-20 08:59:05 +0200

    ft_putstr_fd

--- libft/src/ft_putstr_fd.c
+++ libft/src/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*   By: hrothery <hrothery@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 12:37:08 by cfabian           #+#    #+#             */
-/*   Updated: 2021/12/01 18:28:31 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/20 08:58:46 by hrothery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	if (s)
+		write(fd, s, ft_strlen(s));
 }
