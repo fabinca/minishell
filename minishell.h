d7f76e7a319a46d4e2a7a8cf651695f8d3cb5859
Heather Rothery <hrothery@c2s14d78.42wolfsburg.de>
2022-05-20 09:57:27 +0200

    minishell.h is sorted

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hrothery <hrothery@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 15:37:21 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/20 09:31:33 by hrothery         ###   ########.fr       */
+/*   Updated: 2022/05/20 09:57:10 by hrothery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <limits.h>
-# include </Users/hrothery/goinfre/.brew/opt/readline/include/readline/readline.h>
-# include </Users/hrothery/goinfre/.brew/opt/readline/include/readline/history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <stdlib.h>
 # include "./libft.h"
 # include <stdbool.h>
@@ -85,66 +85,67 @@ typedef struct s_pdata
 	t_envvar	*envlist;
 }	t_pdata;
 
+//builtin_parser.c
+int	builtin_echo(char **cmd, int fd_out);
+int	parse_builtin(t_command *cmd_str, t_envvar *env_list, t_envvar *exp_list);
+
 //builtin_utils.c
 char		*ft_get_envvar(t_envvar *env_list, char *s);
 int			is_alpha_numeric_underscore(char *s);
-void		del_var(t_envvar *lst, int first);
 void		add_export_envvar(t_envvar *lst, char *s);
 void		add_envvar(t_envvar *lst, char *s);
 
 //builtins.c
-int			builtin_echo(char **cmd, int fd);
 int			builtin_env(char **cmd_struct, int fd, t_envvar *list);
 int			builtin_pwd(int fd);
 int			builtin_cd(char **cmd, t_envvar *env_list);
-char		*ft_get_envvar(t_envvar *env_list, char *s);
-
-//builtins2.c
-int			builtin_unset(t_envvar *lst, t_envvar *exp_list, char **cmd);
-int			builtin_export(t_envvar *lst, t_envvar *exp_lst, char **cmd, \
-			int fd);
-int			parse_builtin(t_command *cmd_struct, t_envvar *env_list, \
-			t_envvar *export_list);
-bool		is_builtin(char **cmd);
+int			builtin_exit(t_command *cmd_struct, t_envvar *env_lst, t_envvar *exp_list);
 
 //env_list.c
+t_envvar	*new_var(t_envvar *lst);
 t_envvar	*init_envp_list(char **envp);
 t_envvar	*init_var(t_envvar *var, char *envp);
-t_envvar	*init_export_var(t_envvar *var, char *envp);
 int			search_env_list(t_envvar *lst, char *cmd);
-int			search_exp_list(t_envvar *lst, char *cmd);
-t_envvar	*new_var(t_envvar *lst);
 
 //error_msg.c
 void		print_error(char *str1, char *str2, char *str3, char *str4);
 
-//execute.c
-int			exec_cmd(t_command *cmd_struct, t_envvar *env_list, char **envp);
-
 //exec_utils
-int			exec_cmd(t_command *cmd_struct, t_envvar *env_list, char **envp);
 void		free_my_paths(char **paths);
 char		**find_paths(t_envvar *env_list);
 char		*joined_path(char **my_paths, char *token);
 
+//export_list.c
+t_envvar	*init_export_var(t_envvar *var, char *envp);
+int			search_exp_list(t_envvar *lst, char *cmd);
+
+//export.c
+t_envvar	*export_new_var(t_envvar *lst, t_envvar *new);
+void		print_export_no_args(t_envvar *export_list, int fd);
+int			builtin_export(t_envvar *env_list, t_envvar *exp_list, char **cmd, int fd);
+
 //free.c
-void		free_everything(t_envvar *env, t_envvar *exp, t_command *cmd_s);
-void		free_complete_struct(t_command *cmd_struct);
+void		free_var_list(t_envvar *lst);
 void		free_tokens(t_list *tokens);
 void		free_cmd_struct(t_command *temp);
-int			builtin_exit(t_command *cmd_struct, t_envvar *env, t_envvar *exp);
-void		free_var_list(t_envvar *lst);
-void		free_everything(t_envvar *env_list, t_envvar *exp_list, \
-			t_command *cmd_struct);
+void		free_everything(t_envvar *env, t_envvar *exp, t_command *cmd_s);
+void		free_complete_struct(t_command *cmd_struct);
 
 //get_next_line_delimit
 char		*gnl_delimit(int fd, char *delimiter);
-char		*ft_strjoin_gnl(char *s1, char*s2);
 
 //heredoc.c
 void		exe_heredoc(char *delimiter, t_envvar *env_list);
 
+//init_export_list.c
+t_envvar	*add_var_to_list(t_envvar *lst, t_envvar *new);
+t_envvar	*duplicate_variable(t_envvar *lst, t_envvar *new);
+t_envvar	*create_first_export_var(t_envvar *exp_list);
+t_envvar	*duplicate_list(t_envvar *lst);
+
 //lexer.c
+char		*tok_err(t_list *tokens);
+void		print_token_error(t_list *st);
 t_list		*lexer(char *line);
 
 //list_to_string.c
@@ -152,6 +153,7 @@ char		**ft_listtostr(t_envvar *env_list);
 void		ft_double_free(char **s);
 
 //parser.c
+t_command	*look_for_builtin(t_command *cmd_first, t_command *cmd);
 t_command	*parser(t_list *token, t_envvar *env_list);
 bool		is_builtin(char **cmd);
 
@@ -169,11 +171,10 @@ int			rdr(t_command *cmd, t_list *token, int type, t_envvar *env_list);
 //signals.c
 void		sighandler(int num);
 void		sighandler_child(int num);
-void		display_prompt(void);
 
-//sort_envvars.c
-void		print_export_no_args(t_envvar *lst, int fd);
-t_envvar	*duplicate_list(t_envvar *lst);
-t_envvar	*add_var_to_list(t_envvar *lst, t_envvar *new);
+//unset.c
+void		del_var(t_envvar *lst, int first);
+void		search_variable_to_delete(t_envvar *lst, char *s);
+int			builtin_unset(t_envvar *env_list, t_envvar *exp_list, char **cmd);
 
 #endif
