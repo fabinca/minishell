eb77dce9c111c94aea39bfd8d92585686f7a44d5
Heather Rothery <hrothery@student.42wolfsburg.de>
2022-05-20 10:37:16 +0200

    memory leak gnl fixed

--- src/gnl_delimit.c
+++ src/gnl_delimit.c
@@ -6,7 +6,7 @@
 /*   By: hrothery <hrothery@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 12:47:33 by hrothery          #+#    #+#             */
-/*   Updated: 2022/05/16 11:14:39 by hrothery         ###   ########.fr       */
+/*   Updated: 2022/05/20 10:36:38 by hrothery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,10 @@ static char	*increase_buffer(int fd, char *buf)
 	return (buf);
 }
 
-static char	*get_line(char *buf)
+static void	write_buffer(char *buf, char *out_line)
 {
-	int		i;
-	char	*out_line;
+	int	i;
 
-	i = 0;
-	if (!buf[i])
-		return (0);
-	while (buf[i] != '\n' && buf[i])
-		i++;
-	out_line = malloc(sizeof(char) * (i + 2));
-	if (!out_line)
-		return (0);
 	i = 0;
 	while (buf[i] != '\n' && buf[i])
 	{
@@ -58,6 +49,28 @@ static char	*get_line(char *buf)
 	if (buf[i] == '\n')
 		out_line[i++] = '\n';
 	out_line[i] = '\0';
+}
+
+static char	*get_line(char *buf)
+{
+	int		i;
+	char	*out_line;
+
+	i = 0;
+	if (!buf[i])
+	{
+		free(buf);
+		return (0);
+	}
+	while (buf[i] != '\n' && buf[i])
+		i++;
+	out_line = malloc(sizeof(char) * (i + 2));
+	if (!out_line)
+	{
+		free(buf);
+		return (0);
+	}
+	write_buffer(buf, out_line);
 	free(buf);
 	return (out_line);
 }
@@ -67,14 +80,19 @@ char	*gnl_delimit(int fd, char *delimiter)
 	char		*line;
 	char		*buf;
 
-	buf = NULL;
 	if (fd < 0)
 		return (0);
 	write(1, ">", 1);
-	buf = increase_buffer(fd, buf);
+	buf = increase_buffer(fd, NULL);
 	if (!buf)
 		return (0);
 	line = get_line(buf);
+	if (!line)
+	{
+		print_error("minishell: warning: here-document delimited ", \
+		"by end-of-file (wanted '", delimiter, "')\n");
+		return (NULL);
+	}
 	if (!ft_strcmp(line, "\n"))
 		return (line);
 	if (!ft_strncmp(line, delimiter, ft_strlen(delimiter)) \
